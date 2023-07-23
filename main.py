9e2f8832b2845826e356c0c10858a858c8c9e0be
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-07-23 13:51:26 +0530

    Create main.py

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ source_path = askdirectory(title="Select Source")
 
 files = os.listdir(source_path)
 destination_path = askdirectory(title="Select Destination")
-for file in files:
-    print()
-    print(file)
-    # Create the full source file path
-    source_file_path = os.path.join(source_path, file)
-
-    # Create the full destination file path
-    destination_file_path = os.path.join(destination_path, file)
-
+# Create a new directory
+directory_names = ["PDF", "Video","Zipped Files", "Word Documents", "Excel", "Images"]
+for directory_name in directory_names:
     try:
-        # Copy the file from source to destination
-        shutil.copy(source_file_path, destination_file_path)
-        print(f"Copied: {file}")
-    except PermissionError as e:
-        print(f"Permission denied for {file}. Skipping...")
+        destination_folder = os.path.join(destination_path, directory_name)
+        os.mkdir(destination_folder)
+        print("Directory Created: " + directory_name)
+    except FileExistsError:
+        print("Directory Exists: " + directory_name)
+# for file in files:
+#     print()
+#     print(file)
+#     Create the full source file path
+    # source_file_path = os.path.join(source_path, file)
+    #
+    # Create the full destination file path
+    # destination_file_path = os.path.join(destination_path, file)
+    #
+    # try:
+    #     Copy the file from source to destination
+        # print(f"Copying: {file}")
+        # shutil.copy(source_file_path, destination_file_path)
+        # print(f"Copied: {file}")
+    # except PermissionError as e:
+    #     print(f"Permission denied for {file}. Skipping...")
 
